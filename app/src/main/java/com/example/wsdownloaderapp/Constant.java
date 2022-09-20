58af0fc846099f7a9ae25f9de3af0f431484cb04
Dol arnold <zedekiadol@gmail.com>
2022-09-20 14:52:58 +0300

    Story Adapter added

---
+++ app/src/main/java/com/example/wsdownloaderapp/Constant.java
@@ -0,0 +1,6 @@
+package com.example.wsdownloaderapp;
+
+public class Constant {
+    public static final String FOLDER_NAME ="/whatsapp";
+    public static final String  SAVE_FOLDER_NAME ="/WS Downloader";
+}
